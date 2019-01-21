da5912c15c26144d151c19874151a143226ae7a5
Frances Sin <frances.sin31@gmail.com>
2019-01-20 18:00:52 -0800

    fixed gui

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0x6e8bd1);     
 canvas.appendChild(renderer.domElement);
 
+
 //////////////////////////////////////////////////////////////////////////////////////
 // Set up camera
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -88,7 +89,7 @@ function spinMesh() {
     spinning.push(meshes[meshes_size-1]);
   }
 }
- 
+
 
 function createGeometry(geometry,colour,xpos,ypos) {
   if (geometry != null) {
@@ -121,10 +122,15 @@ floor.position.y = -1.1;
 floor.rotation.x = Math.PI / 2;
 scene.add(floor);
 
-
+// Add grid to platform:
 var divisions = 15;
 var gridHelper = new THREE.GridHelper( size, divisions );
 scene.add( gridHelper );
+
+// Add world coordinate axes
+var worldFrame = new THREE.AxesHelper(5) ;
+scene.add(worldFrame);
+
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Set up lights
@@ -136,44 +142,98 @@ light.position.set(0,4,2);
 scene.add(light);
 var vcsLight = new THREE.Vector3(light.position);
 
-
-
-sphereGeometry = new THREE.SphereGeometry(0.3, 32, 32);    // radius, segments, segments
+// Sphere representing light:
+sphereGeometry = new THREE.SphereGeometry(0.3, 32, 32);  
 lightSphere = new THREE.Mesh(sphereGeometry, new THREE.MeshBasicMaterial( {color: 0xf6f0b6} ));
 lightSphere.position.set(light.position.x, light.position.y, light.position.z);
 scene.add(lightSphere);
 
-boxGeometry = new THREE.BoxGeometry( 2, 2, 2 );
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Box
+///////////////////////////////////////////////////////////////////////////////////////
+boxGeometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
 box = new THREE.Mesh( boxGeometry, new THREE.MeshLambertMaterial( {color: 0xcccccc} ) );
-box.position.set(-4, 1, 0);
+box.position.set(-4, 0.8, 0);
 scene.add( box );
+meshes.push(box);
+meshes_size++;
 
-
-
-////////////////////// TOON SHADER /////////////////////////////
-var toonMaterial = new THREE.ShaderMaterial( {
-        uniforms: {
-           lightPosition: {value: new THREE.Vector3(0.0,0.0,-1.0) },
-           myColor: { value: new THREE.Vector4(0.79,0.79,0.83,1.0) }
-        },
-  vertexShader: document.getElementById( 'myVertShader').textContent,
-  fragmentShader: document.getElementById( 'toonFragShader' ).textContent
-} );
-///////////////////////////////////////////////////////////////
-
-var geometry = new THREE.TorusKnotGeometry(1, 0.4, 64, 8, 2, 3);
-var torusKnot = new THREE.Mesh( geometry, toonMaterial );
-torusKnot.position.set(-3,1.75,-3);
-scene.add( torusKnot );
-
-
-
-var worldFrame = new THREE.AxesHelper(5) ;
-scene.add(worldFrame);
 
 
 //////////////////////////////////////////////////////////////////////////////////////
-// LISTEN TO KEYBOARD
+// Toon shader
+///////////////////////////////////////////////////////////////////////////////////////
+var toonMaterial = new THREE.ShaderMaterial( {
+  uniforms: {
+   lightPosition: {value: new THREE.Vector3(0.0,0.0,-1.0) },
+   myColor: { value: new THREE.Vector4(0.79,0.79,0.83,1.0) }
+ },
+ vertexShader: document.getElementById( 'myVertShader').textContent,
+ fragmentShader: document.getElementById( 'toonFragShader' ).textContent
+} );
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Torus knot 
+///////////////////////////////////////////////////////////////////////////////////////
+var geometry = new THREE.TorusKnotGeometry(1, 0.4, 64, 8, 2, 3);
+var torusKnot = new THREE.Mesh( geometry, toonMaterial );
+torusKnot.position.set(-4.5,2.25,-3);
+torusKnot.scale.set(1.25,1.25,1.25);
+scene.add( torusKnot );
+meshes.push(torusKnot);
+meshes_size++;
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//  Standford bunny
+/////////////////////////////////////////////////////////////////////////////////////
+var bunnyMaterial = new THREE.ShaderMaterial( {
+
+  vertexShader: document.getElementById( 'bunnyVertexShader' ).textContent,
+  fragmentShader: document.getElementById( 'bunnyFragmentShader' ).textContent
+} );
+
+var manager = new THREE.LoadingManager();
+manager.onProgress = function ( item, loaded, total ) {
+  console.log( item, loaded, total );
+};
+
+var bunnyTexture = bunnyMaterial;
+
+var onProgress = function ( xhr ) {
+  if ( xhr.lengthComputable ) {
+    var percentComplete = xhr.loaded / xhr.total * 100;
+    console.log( Math.round(percentComplete, 2) + '% downloaded' );
+  }
+};
+
+var onError = function ( xhr ) {
+};
+
+var loader = new THREE.OBJLoader( manager );
+loader.load( '/meshes/stanford_bunny.obj', function ( object ) {
+  object.traverse( function ( child ) {
+    if ( child instanceof THREE.Mesh ) {
+      child.material = bunnyTexture;
+    }
+  });
+  scene.add( object );
+  meshes.push(object);
+  meshes_size++;
+  object.scale.set(7.25,7.25,7.25);
+  object.position.x = box.position.x;
+  object.position.y = 1.3;
+  object.position.z = box.position.z;
+}, onProgress, onError );
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Keyboard listener
 ///////////////////////////////////////////////////////////////////////////////////////
 
 var keyboard = new THREEx.KeyboardState();
@@ -186,7 +246,7 @@ function checkKeyboard() {
     console.log('W pressed');
     light.position.y += 0.1;
   } else if (keyboard.pressed("S"))
-    light.position.y -= 0.1;
+  light.position.y -= 0.1;
   if (keyboard.pressed("A"))
     light.position.x -= 0.1;
   else if (keyboard.pressed("D"))
@@ -195,25 +255,58 @@ function checkKeyboard() {
   lightSphere.position.set(light.position.x, light.position.y, light.position.z);
 
   if (spinning.length != 0) {
-
-
     spinning.forEach(function(mesh) {
-   mesh.rotation.y += 0.01;
- });
+      mesh.rotation.y += 0.01;
+    });
+  }
 }
 
-}
+var gui = new dat.GUI();
+
+var guiCommandsFolder = gui.addFolder('Commands');
+
+var commands = {
+  clear: function() {
+    clear();
+  },
+  delete: function() {
+    undo();
+  },
+  spin: function() {
+    spinMesh();
+  },
+  sphere: function() {
+    createGeometry('SphereGeometry',0,0,1);
+  },
+  box: function() {
+    createGeometry('BoxGeometry',0,0,1);
+  },
+  torusKnot: function() {
+    createGeometry('TorusKnotGeometry',0,0,1);
+  }
+};
+
+guiCommandsFolder.add( commands, 'delete' );
+guiCommandsFolder.add( commands, 'clear' );
+guiCommandsFolder.add( commands, 'spin' );
+var guiCreateFolder = guiCommandsFolder.addFolder('create');
+guiCreateFolder.add( commands, 'sphere');
+guiCreateFolder.add( commands, 'box');
+guiCreateFolder.add( commands, 'torusKnot');
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////
-// UPDATE CALLBACK
+// Update callback function
 ///////////////////////////////////////////////////////////////////////////////////////
 function update() {
   checkKeyboard();
-  requestAnimationFrame(update);      // requests the next update call;  this creates a loop
+  requestAnimationFrame(update);      // request the next update call
   renderer.render(scene, camera);
 
-
+  for (var i in gui.__controllers) {
+    gui.__controllers[i].updateDisplay();
+  }
 }
 
 update();
